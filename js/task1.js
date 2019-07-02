973a49f9fe4f90441138cd9954e54347105d18bc
Amir Raj Gurung <amir_grg@hotmail.com>
2019-07-02 15:05:31 +0545

    we added equal condition with if else

--- js/task1.js
+++ js/task1.js
@@ -1,10 +1,13 @@
  var firstNumber= prompt("add number");
  var secNumber = prompt("add second number");
 
- 
+ firstNumber= parseInt(firstNumber);
+ secNumber= parseInt(secNumber);
 
  if( firstNumber > secNumber){
-     console.log("first number is greater", typeof firstNumber)
+     console.log("first number is greater", typeof firstNumber);
+ }else if(firstNumber==secNumber){
+     console.log("two numbers are equal", typeof secNumber);
  }else{
-     console.log("second numberis greater", typeof secNumber)
+     console.log("second number is greater");
  }
