3d52433552572e9a65179954f9264bb1e6053fec
Amir Raj Gurung <amir_grg@hotmail.com>
2019-07-03 14:06:12 +0545

    loop

---
+++ js/loop.js
@@ -0,0 +1,19 @@
+for (var i =0; i < 10 ; i++){
+    console.log("hello", i); 
+   if(i % 2 == 0){
+       console.log("is divisible");
+   } 
+   
+}
+
+
+var initializer = 4;
+while(initializer < 10){
+    console.log("hello from while loop", initializer); 
+    if(initializer % 4 ==     0){
+        console.log("is divisible from while loop");
+
+    }
+    initializer=initializer+2
+}
+
